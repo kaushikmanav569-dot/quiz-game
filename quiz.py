2e3c6ace135a7186de364ae167772c16995bc7b6
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 16:28:16 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,16 +1,38 @@
 import streamlit as st
+st.title("QUIZ GAME:-")
+ques=["Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f","Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale","Q3: Which is the largest ocean in the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean","Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant","Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9"]
+AL=[]
+for i in ques:
+    st.write(i)
+    ans =st.text_input(f"Enter your choice....",key=i)
+    AL.append(ans)
+    st.write("<....................................>")
+# for j in range(len(ques)):
+#     ans =st.text_input(f"Enter your {j+1} choice....",key=i)
+#     AL.append(ans)
+# st.write(AL)
+# RA=['c','d','b','c','b']
+Total = 0
+if AL[0] == 'c' or AL[0]=="C":
+    Total +=1
+    
+if AL[1] == 'd' or AL[1]=="D":
+    Total +=1
 
-st.title(" Quiz Game")
-st.write("Answer the following 5 questions:")
+if AL[2] == 'b' or AL[2]=="B":
+    Total +=1
 
-st.write("ques1>what the natinal animal?")
-answer=st.text_input("enter the choice(tiger/cow/lion/horse):")
-st.write("ques2>what the natinal flower?")
-answer=st.text_input("enter the choice(lotus/sun flower/rose):")
-st.write("ques3>what the total bones?")
-answer=st.text_input("enter the choice(206/560/216/234):")
-st.write("ques4>what the friend name?")
-answer=st.text_input("enter the choice(vishal/monu/sagar/lokesh):")
-st.write("ques5>what the follower in instragram?")
-answer=st.text_input("enter the choice(334/7657/57/876):")
-pip install streamlit
+if AL[3] == 'c' or AL[3]=="C":
+    Total +=1
+
+if AL[4] == 'b' or AL[4]=="B":
+    Total +=1
+if Total ==5:
+    st.write("Congratulations.... You are the topper with 5 points....")
+elif Total == 4:
+    st.write("Congratulations... You have scored Second position")
+elif Total == 3:
+    st.write("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
+else:
+    st.write("Better Luck next time....")
+st.balloons()
