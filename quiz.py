01a8cd7d52bc92c464f48d578bbea9583e6477de
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 12:19:06 +0530

    Update quiz questions
    Removed the first question from the quiz and added a new question about the national animal.

--- quiz.py
+++ quiz.py
@@ -3,6 +3,4 @@ import streamlit as st
 st.title("🎯 Quiz Game")
 st.write("Answer the following 5 questions:")
 
-questions = [
-    "Q1: First Alphabet of English language?\nA) D\tB) E\tC) A\tD) F",
-]
+st.write("what the natinal animal?")
