5f7e58e7d594e58040ec374dd916ff2fa6cf2ade
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 12:09:59 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,23 +1,22 @@
+
 import streamlit as st
 
-st.title("🎯 Simple Quiz ")
-st.write("Q1. What is the first alphabet of English language?")
-answer1 = st.selectbox("Choose your answer:", ["A", "B", "C", "D"])
-if answer1 == "A":
-    score += 1
+st.title("🎯 Quiz Game")
+st.write("Answer the following 5 questions:")
 
-# Question 2
-st.write("Q2. Which of these is a mammal?")
-answer2 = st.selectbox("Choose your answer:", ["Duck", "Hen", "Owl", "Whale"])
-if answer2 == "Whale":
-    
+questions = [
+    "Q1: First Alphabet of English language?\nA) D\tB) E\tC) A\tD) F",
+    "Q2: Which of these is a mammal?\nA) Duck\tB) Hen\tC) Owl\tD) Whale",
+    "Q3: Which is the largest ocean in the world?\nA) Indian\tB) Pacific\tC) Arctic\tD) Atlantic",
+    "Q4: Which is the National Animal of India?\nA) Fox\tB) Cow\tC) Tiger\tD) Elephant",
+    "Q5: How many continents are there in the world?\nA) 4\tB) 7\tC) 8\tD) 9"
+]
 
-# Show result
-st.write("---")
-st.write(f"Your total score is: *{score}*")
+answers = ['C', 'D', 'B', 'C', 'B']
+user_answers = []
 
-
-    st.success("🎉 Congratulations  You got all answers correct!")
-    st.balloons()
-else:
-    st.info("Try again Ayaan! You can do better 🙂")
+# Ask questions using radio buttons
+for i, q in enumerate(questions):
+    st.subheader(q)
+    ans = st.radio(f"Select your answer for Question {i+1}:", ['A', 'B', 'C', 'D'], key=i)
+    user…
