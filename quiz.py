2ede8cf4c9570e3b35fde74c4c02002c008486d4
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 13:09:48 +0530

    Add additional questions to the quiz game

--- quiz.py
+++ quiz.py
@@ -4,6 +4,14 @@ st.title("🎯 Quiz Game")
 st.write("Answer the following 5 questions:")
 
 st.write("ques1>what the natinal animal?")
-answer=st.text_input("enter the choice(tiger/cow/lion/horse):)")
-st.ballons()
+answer=st.text_input("enter the choice(tiger/cow/lion/horse):")
+st.write("ques2>what the natinal flower?")
+answer=st.text_input("enter the choice(lotus/sun flower/rose):")
+st.write("ques3>what the total bones?")
+answer=st.text_input("enter the choice(206/560/216/234):")
+st.write("ques4>what the friend name?")
+answer=st.text_input("enter the choice(vishal/monu/sagar/lokesh):")
+st.write("ques5>what the follwer in instragram?")
+answer=st.text_input("enter the choice(334/7657/57/876):")
+
   
