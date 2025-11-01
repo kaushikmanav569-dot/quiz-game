6a6b42f4956d58f1f6e2ee4b237843bf6b82a8fa
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 10:36:55 +0530

    Implement quiz functionality in quiz.py
    Added a quiz with multiple questions and choices.

--- quiz.py
+++ quiz.py
@@ -3,3 +3,29 @@ import streamlit as st
 name = st.text_input("Enter your name:")
 st.write("Hello,", name)
 st.balloons()
+Q1: First Alphabet of English language..?
+A) d	B) e
+C) a	D) f
+Enter your choice....c
+<....................................>
+Q2: Which of these is a mammel..?
+A) Duck	B) Hen
+C) Owl	D) Whale
+Enter your choice....d
+<....................................>
+Q3: Which is the largest ocean in the world?
+A) Indian ocean	B) Pacific ocean
+C) Arctic ocean	D) Atlantic ocean
+Enter your choice....b
+<....................................>
+Q4: Which is the National Animal ..?
+A) Fox	B) Cow
+C) Tiger	D) Elephant
+Enter your choice....c
+<....................................>
+Q5: How many continents in the world..?
+A) 4	B) 7
+C) 8	D) 9
+Enter your choice....b
+<....................................>
+Congratulations.... You are the topper with 5 points....
