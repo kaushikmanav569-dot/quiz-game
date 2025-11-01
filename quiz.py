07353148f176a06fc94e87af77b9cb782cbeb51b
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 12:28:21 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("🎯 Quiz Game")
 st.write("Answer the following 5 questions:")
 
-st.write("what the natinal animal?")
+st.write("ques1>what the natinal animal?")
+answer=st.text_input("enter the choice(tiger/cow/lion/horse):)")
