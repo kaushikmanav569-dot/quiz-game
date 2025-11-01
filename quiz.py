f7d17fd6de838fb5a4ebd2a1818beecebaa57bb8
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 10:49:51 +0530

    Update quiz title and remove user-specific messages

--- quiz.py
+++ quiz.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
-st.title("🎯 Simple Quiz Game for Ayaan")
-
-score = 0  # shuru me score 0 rakha
-
-# Question 1
+st.title("🎯 Simple Quiz ")
 st.write("Q1. What is the first alphabet of English language?")
-answer1 = st.radio("Choose your answer:", ["A", "B", "C", "D"])
+answer1 = st.selectbox("Choose your answer:", ["A", "B", "C", "D"])
 if answer1 == "A":
     score += 1
 
 # Question 2
 st.write("Q2. Which of these is a mammal?")
-answer2 = st.radio("Choose your answer:", ["Duck", "Hen", "Owl", "Whale"])
+answer2 = st.selectbox("Choose your answer:", ["Duck", "Hen", "Owl", "Whale"])
 if answer2 == "Whale":
     score += 1
 
@@ -21,7 +17,7 @@ st.write("---")
 st.write(f"Your total score is: *{score}*")
 
 if score == 2:
-    st.success("🎉 Congratulations Ayaan! You got all answers correct!")
+    st.success("🎉 Congratulations  You got all answers correct!")
     st.balloons()
 else:
     st.info("Try again Ayaan! You can do better 🙂")
