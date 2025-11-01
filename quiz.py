d8a5fda899c52c0db0bab81c3f0eecef90fd6445
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 15:59:59 +0530

    Revise quiz game questions and title
    Updated quiz questions and improved formatting.

--- quiz.py
+++ quiz.py
@@ -1,9 +1,15 @@
 import streamlit as st
- st.title("Quiz Game:-")
- st.write("Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f")
- ans1 = st.text_input("Enter your answer here...",key.i)
- st.write("your answer is ...",ans1)
- st.write("<....................................>")
- st.write("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
- ans2 =st.text_input("Enter your answer here...")# print("your answer is ...",ans2)
- st.write("<...................................>")
+
+st.title("🎯 Quiz Game")
+st.write("Answer the following 5 questions:")
+
+st.write("ques1>what the natinal animal?")
+answer=st.text_input("enter the choice(tiger/cow/lion/horse):")
+st.write("ques2>what the natinal flower?")
+answer=st.text_input("enter the choice(lotus/sun flower/rose):")
+st.write("ques3>what the total bones?")
+answer=st.text_input("enter the choice(206/560/216/234):")
+st.write("ques4>what the friend name?")
+answer=st.text_input("enter the choice(vishal/monu/sagar/lokesh):")
+st.write("ques5>what the follwer in instragram?")
+answer=st.text_input("enter the choice(334/7657/57/876):")
