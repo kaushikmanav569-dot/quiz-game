b802bea1fe82bafdc163221724fb5e0362a9fdfe
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 13:25:53 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
  import streamlit as it
  st.title("Quiz Game:-")
  st.write("Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f")
- ans1 = st.text_input("Enter your answer here...")
+ ans1 = st.text_input("Enter your answer here...",key.i)
  st.write("your answer is ...",ans1)
  st.write("<....................................>")
  st.write("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
