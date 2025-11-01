31c94e736b1370c692389bc078d433a8eeca2f50
kaushikmanav569-dot <kaushikmanav569@gmail.com>
2025-11-01 13:19:50 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,17 +1,39 @@
-import streamlit as st
-
-st.title("🎯 Quiz Game")
-st.write("Answer the following 5 questions:")
-
-st.write("ques1>what the natinal animal?")
-answer=st.text_input("enter the choice(tiger/cow/lion/horse):")
-st.write("ques2>what the natinal flower?")
-answer=st.text_input("enter the choice(lotus/sun flower/rose):")
-st.write("ques3>what the total bones?")
-answer=st.text_input("enter the choice(206/560/216/234):")
-st.write("ques4>what the friend name?")
-answer=st.text_input("enter the choice(vishal/monu/sagar/lokesh):")
-st.write("ques5>what the follwer in instragram?")
-answer=st.text_input("enter the choice(334/7657/57/876):")
-
-  
+ Quiz Game:-
+ print("Q1: First Alphabet of English language..?\nA) d\tB) e\nC) a\tD) f")
+ ans1 = input("Enter your answer here...")
+ print("your answer is ...",ans1)
+ print("<....................................>")
+ print("Q2: Which of these is a mammel..?\nA) Duck\tB) Hen\nC) Owl\tD) Whale")
+ ans2 = input("Enter your answer here...")# print("your answer is ...",ans2)
+ print("<...................................>")
+ print("Q3: Which is the largest ocean n the world?\nA) Indian ocean\tB) Pacific ocean\nC) Arctic ocean\tD) Atlantic ocean")
+ ans3 = input("Enter your answer here...")
+ print("your answer is ...",ans3)
+ print("<....................................>")
+ print("Q4: WHich is the National Animal ..?\nA) Fox\tB) Cow\nC) Tiger\tD) Elephant")
+ ans4 = input("Enter your answer here...")
+ print("your answer is ...",ans4)
+ print("<....................................>")
+ print("Q5: How many continents in the world..?\nA) 4\tB) 7\nC) 8\tD) 9")
+ ans5 = input("Enter your answer here...")
+ print("your answer is ...",ans5)
+ Total = 0
+ if ans1=="c" or ans1 == "C":
+     Total +=1
+if ans2=="d" or ans2 == "D":
+     Total +=1
+ if ans3=="b" or ans3 == "B":
+     Total +=1
+ if ans4=="c" or ans4 == "C":
+     Total +=1
+ if ans5=="b" or ans5 == "B":
+     Total +=1
+ print(Total)
+ if Total ==5:
+     print("Congratulations.... You are the topper with 5 points....")
+ elif Total == 4:
+     print("Congratulations... You have scored Second position")
+ elif Total == 3:
+     print("You have passed the Quiz wih 3 points.. Wish you very best for next time....  ")
+ else:
+     print("Better Luck next time....")
